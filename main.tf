9619d8e8c437610cb32eea4b5063cd28710bad4a
saipavan1994 <vsaipavanreddy1994@gmail.com>
2024-08-08 12:54:11 +0530

    Update main.tf by pavan

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 resource "aws_instance" "pavaninfra" {
 	ami = "ami-0c2b8ca1dad447f8a" 
 	instance_type = "t2.micro"
-tag = {
-Name = "pavan-cloud"
+tags = {
+        Name = "pavan-cloud"
 }
 }
