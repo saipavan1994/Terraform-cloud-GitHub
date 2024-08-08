351cfc066500aa7122edb38fa330d86e28c7a38d
saipavan1994 <vsaipavanreddy1994@gmail.com>
2024-08-08 12:52:51 +0530

    Update main.tf replaced nano with micro

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 
 resource "aws_instance" "pavaninfra" {
 	ami = "ami-0c2b8ca1dad447f8a" 
-	instance_type = "t2.nano"
+	instance_type = "t2.micro"
 tag = {
 Name = "pavan-cloud"
 }
